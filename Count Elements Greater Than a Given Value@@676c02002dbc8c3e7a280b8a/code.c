c4235c7f820a8079ddeb1a76836604716be6fcee
Ayush <ayushsinghbangari@gmail.com>
2025-04-11 09:26:53 +0530

    Solution to Count Elements Greater Than a Given Value

---
+++ Count Elements Greater Than a Given Value@@676c02002dbc8c3e7a280b8a/code.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+int n;
+scanf("%d",&n);
+count =0;
+int arr[n];
+for (int i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+}
+int k;
+scanf("%d",&k);
+
+for (int i=0;i<n;i++){
+    if(arr[i]>k){
+        count++;
+    }
+    printf("%d",count);
+}
+    return 0;
+}
